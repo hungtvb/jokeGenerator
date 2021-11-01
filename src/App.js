3ac7cd7df87093f39171af04292b847306e28eaa
hungtvb <hung.tvb.2020@gmail.com>
2021-11-01 12:10:35 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+import useRandomJoke from './useRandomJoke';
+
+function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const [firstName, setFirstName] = useState("Tran");
+  const [lastName, setLastName] = useState("Hung");
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = () => {
+    if(!firstName || !lastName){
+      return false;
+    }
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  }
+
+  return (
+    <div className="app">
+      <h1>The Joke Generator</h1>
+      <q>{joke}</q>
+      <form className="form">
+        <input placeholder="First Name" ref={firstNameRef}/>
+        <input placeholder="Last Name" ref={lastNameRef}/>
+      </form>
+      <button className="button" onClick={generateJoke}>Generate Joke</button>
+    </div>
+  );
+}
+
+export default App;
